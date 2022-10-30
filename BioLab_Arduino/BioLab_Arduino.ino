c1484456dbd3f2d6fb840f2e0e40bf7cf2b3a57c
ikashii <judka94@gmail.com>
2022-10-30 17:53:00 +0100

    2 Chanell auf 3 Chanell

--- BioLab_Arduino/BioLab_Arduino.ino
+++ BioLab_Arduino/BioLab_Arduino.ino
@@ -1,39 +1,78 @@
-#include <MCP3202.h>
 #include <FlexiTimer2.h>
-#define DATA_DIVIDER_BYTE1 120
+#include <SPI.h>
 
+//     MCP3204 -- Arduino
+//Chipselect 8 -- D10
+//       DIN 9 -- D11
+//     DOUT 10 -- D12
+//      CLK 11 -- D13
 
-int Vdd = 5; //change according to your Vdd value.
+// Set Constants
+const int CS = 10;      // set pin 10 as the chip select for the ADC:
 
-MCP3202 adc = MCP3202(10); //Parameter passed is the CS pin number. Change according to requirement.
+typedef struct
+{
+    uint32_t sampleIndex;            // Number of sample
+    uint16_t ch0, ch1, ch2;            // Sample values
+    
+} DataPackage;
 
-long int index = 0; 
-
+int8_t dataDividerByte = 120;    // Separator
+uint32_t index = 0;
 
 void flash() 
 {
+  DataPackage package;
+  package.sampleIndex = index;
+  package.ch0 = readAdc(0);
+  package.ch1 = readAdc(1);
+  package.ch2 = readAdc(2);
 
-  float x = (float)index;
-  float ch1 = (adc.readChannel(0)*Vdd)/4096.0 + 1.0f;
-  float ch2 = (adc.readChannel(1)*Vdd)/4096.0;
-
-  float vals[3] = { x, ch1, ch2 };
-  Serial.write(120);
-  Serial.write((byte*)&vals[0], 12);
-  
-
+  Serial.write(dataDividerByte);
+  Serial.write((byte*)&package, 11);    //11 Bit, 88 Bytes
   index++;
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  adc.begin();
-  Serial.begin(1000000);  
+// Start setup function:
+void setup() {  
   
+  Serial.begin(1000000);
+  pinMode (CS, OUTPUT); 
+  
+  // set the ChipSelectPins high initially: 
+  digitalWrite(CS, HIGH); 
+   
+  // initialise SPI:
+  SPI.beginTransaction(SPISettings(1400000, MSBFIRST, SPI_MODE3));
+  SPI.begin();
+
   FlexiTimer2::set(1, 1.0/2000.0, flash); // 2000x per second T = 0.5ms
   FlexiTimer2::start();
-}
 
-void loop() {
+  
+} // End setup function.
 
+// Start loop function:
+void loop() {  
+  
+}// End of loop function.
+
+//Function to read the ADC, accepts the channel to be read.
+int16_t readAdc(int channel) {
+  byte adcPrimaryRegister = 0b00000110;      // Sets default Primary ADC Address register B00000110, This is a default address setting, the third LSB is set to one to start the ADC, the second LSB is to set the ADC to single ended mode, the LSB is for D2 address bit, for this ADC its a "Don't Care" bit.
+  byte adcPrimaryRegisterMask = 0b00000111;  // b00000111 Isolates the three LSB.  
+  byte adcPrimaryByteMask = 0b00001111;      // b00001111 isolates the 4 LSB for the value returned. 
+  byte adcPrimaryConfig = adcPrimaryRegister & adcPrimaryRegisterMask; // ensures the adc register is limited to the mask and assembles the configuration byte to send to ADC.
+  byte adcSecondaryConfig = channel << 6;
+  noInterrupts(); // disable interupts to prepare to send address data to the ADC.  
+  digitalWrite(CS,LOW); // take the Chip Select pin low to select the ADC.
+  SPI.transfer(adcPrimaryConfig); //  send in the primary configuration address byte to the ADC.  
+  byte adcPrimaryByte = SPI.transfer(adcSecondaryConfig); // read the primary byte, also sending in the secondary address byte.  
+  byte adcSecondaryByte = SPI.transfer(0x00); // read the secondary byte, also sending 0 as this doesn't matter. 
+  digitalWrite(CS,HIGH); // take the Chip Select pin high to de-select the ADC.
+  interrupts(); // Enable interupts.
+  adcPrimaryByte &= adcPrimaryByteMask; // Limits the value of the primary byte to the 4 LSB:
+  int16_t digitalValue = (adcPrimaryByte << 8) | adcSecondaryByte; // Shifts the 4 LSB of the primary byte to become the 4 MSB of the 12 bit digital value, this is then ORed to the secondary byte value that holds the 8 LSB of the digital value.
+  //float value = (float(digitalValue) * 5.000) / 4096.000; // The digital value is converted to an analogue voltage using a VREF of 2.048V.
+  return digitalValue; // Returns the value from the function
 }
